d8a37c8dcf45597ff1b754e201917877ab9fb9d4
thedarkleaves <thedar.kleaves@zohomail.com.au>
2020-04-30 14:24:01 +1200

    debugging

--- www/js/core.js
+++ www/js/core.js
@@ -835,6 +835,7 @@ function makePainDiary4() {
 }
 
 function makeMedDiary() {
+    try {
     // med diary 1
     $("#meddiary1").html('<span class="question">which medications did you use?</span><br>');
     for (i=0;i<meds.medication.length;i++) {
@@ -848,6 +849,9 @@ function makeMedDiary() {
 
     $(".toggle.dose").hide();
     $(".mednum").hide();
+    } catch(err) {
+        printdebug(err.message);
+    } 
     function applymedtoggleclick () {
         $("#meddiary1 .toggle").off("click");
         $("#meddiary1 .toggle").click(function(){
@@ -887,6 +891,8 @@ function makeMedDiary() {
             }
         });
     }
+    
+
     applymedtoggleclick();
 
     $("#finishmed1").click(function(){
