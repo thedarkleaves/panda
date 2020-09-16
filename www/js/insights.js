9ec25e9e1eba48750b266f7de844cc765114f0aa
thedarkleaves <thedar.kleaves@zohomail.com.au>
2020-09-17 09:40:26 +1200

    formatting on insights

--- www/js/insights.js
+++ www/js/insights.js
@@ -109,6 +109,7 @@ function printInsights(elementToPrintTo) {
             var noSE = painSD / Math.sqrt(factorsInsightsMatrix[i][noSum]);
             var noMean = factorsInsightsMatrix[i][noSum] / factorsInsightsMatrix[i][noCount];
         
+            
             if (yesMean>noMean) {
                 // pain is higher when the factor is yes
                 if ((yesMean-confidenceStat*yesSE) > (noMean+confidenceStat*noSE)) {
